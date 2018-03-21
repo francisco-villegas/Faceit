604d0adc6d845e2969e9a3a66209c1aec1ce79bb
Francisco Villegas <francisco.alejandre@volvocars.com>
2018-03-20 23:56:32 -0700

    Lesson 5 from: https://www.youtube.com/watch?v=FXinJu_NkWU&index=5&list=PLPA-ayBrweUz32NSgNZdl0_QISw-f12Ai Including multiple mvcd implementations and IBInspectable UI observers.

--- Faceit/FaceView.swift
+++ Faceit/FaceView.swift
@@ -10,20 +10,64 @@ import UIKit
 
 @IBDesignable
 class FaceView: UIView {
-    @IBInspectable
-    var scale: CGFloat = 0.9
+    
+    //Public API
     
     @IBInspectable
-    var eyesOpen: Bool = true
+    var scale: CGFloat = 0.9 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
     
     @IBInspectable
-    var lineWidth: CGFloat = 5.0
-
-    @IBInspectable
-    var color: UIColor = UIColor.blue
+    var eyesOpen: Bool = true {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
     
     @IBInspectable
-    var mouthCurvature: Double = -0.5 // 1.0 is full smile and -1.0 is full frown
+    var lineWidth: CGFloat = 5.0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable
+    var color: UIColor = UIColor.blue {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    // 1.0 is full smile and -1.0 is full frown
+    @IBInspectable
+    var mouthCurvature: Double = -0.5 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @objc func changeScale(byReactingTo pinchRecognizer: UIPinchGestureRecognizer) {
+        switch pinchRecognizer.state {
+        case .changed, .ended:
+            scale *= pinchRecognizer.scale
+            pinchRecognizer.scale = 1
+        default:
+            break
+        }
+    }
+    
+    // Private Implementation
+    
+    private struct Ratios {
+        static let skullRadiusToEyeOffset: CGFloat = 3
+        static let skullRadiusToEyeRadius: CGFloat = 10
+        static let skullRadiusToMouthWidth: CGFloat = 1
+        static let skullRadiusToMouthHeight: CGFloat = 3
+        static let skullRadiusToMouthOffset: CGFloat = 3
+    }
     
     private var skullRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
@@ -81,7 +125,7 @@ class FaceView: UIView {
         
         let cp1 = CGPoint(x: start.x + mouthRect.width / 3, y: start.y + smileOffset)
         let cp2 = CGPoint(x: end.x - mouthRect.width / 3, y: start.y + smileOffset)
-
+        
         let path = UIBezierPath()
         path.move(to: start)
         path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
@@ -103,14 +147,6 @@ class FaceView: UIView {
         pathForEye(.left).stroke()
         pathForEye(.right).stroke()
         pathForMouth().stroke()
-    }
-    
-    private struct Ratios {
-        static let skullRadiusToEyeOffset: CGFloat = 3
-        static let skullRadiusToEyeRadius: CGFloat = 10
-        static let skullRadiusToMouthWidth: CGFloat = 1
-        static let skullRadiusToMouthHeight: CGFloat = 3
-        static let skullRadiusToMouthOffset: CGFloat = 3
     }
     
 }
